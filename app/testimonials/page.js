4ce646024091745da9e5da8d531dd960c8b59547
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-10-18 11:02:10 +0200

    Create testimonials page

---
+++ app/testimonials/page.js
@@ -0,0 +1,118 @@
+import MainHeader from '@/components/MainHeader';
+import SideNav from '@/components/SideNav';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
+import Heading from '@/components/Heading';
+import MainFooter from '@/components/MainFooter';
+
+const page = () => {
+  return (
+    <>
+      <MainHeader />
+      <SideNav />
+      <main className={styles.main}>
+        <Heading text="Αναγνώριση" />
+        <section className={styles.testimonials} id="testimonials">
+          <div className={styles['testimonials-box']}>
+            <div className={styles.wrapper}>
+              <div className={styles['testimonial-item']}>
+                <Image
+                  className={styles.img}
+                  src={'/images/mina-rad-K9T9hdf4PmI-unsplash.jpg'}
+                  height={300}
+                  width={300}
+                  alt="Εικόνα Άνδρα"
+                />
+                <h2>Αλέξανδρος</h2>
+                <div className={styles.rating}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
+                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
+                </p>
+              </div>
+
+              <div className={styles['testimonial-item']}>
+                <Image
+                  className={styles.img}
+                  src={'/images/γυναίκα.png'}
+                  height={300}
+                  width={300}
+                  alt="Εικόνα Γυναίκας"
+                />
+                <h2>Υπατία</h2>
+                <div className={styles.rating}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
+                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
+                </p>
+              </div>
+
+              <div className={styles['testimonial-item']}>
+                <Image
+                  className={styles.img}
+                  src={'/images/mina-rad-K9T9hdf4PmI-unsplash.jpg'}
+                  height={300}
+                  width={300}
+                  alt="Εικόνα Άνδρα"
+                />
+                <h2>Σωκράτης</h2>
+                <div className={styles.rating}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
+                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
+                </p>
+              </div>
+
+              {/* <div className={styles['testimonial-item']}>
+                <Image
+                  className={styles.img}
+                  src={'/images/γυναίκα.png'}
+                  height={300}
+                  width={300}
+                  alt="Εικόνα Γυναίκας"
+                />
+                <h2>Αθηνά</h2>
+                <div className={styles.rating}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
+                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
+                </p>
+              </div> */}
+            </div>
+          </div>
+        </section>
+      </main>
+      <MainFooter />
+    </>
+  );
+};
+export default page;
