3486f33c9676fc07152fe3f8b435c6207150ada2
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-11-15 17:34:35 +0100

    Update Education Page

--- app/programming/education/page.js
+++ app/programming/education/page.js
@@ -2,20 +2,20 @@ import Heading from '@/components/Heading';
 import TimelineItems from '@/components/TimelineItems';
 import styles from './page.module.css';
 
-
-
-export default function Home() {
+const Education = () => {
   return (
     <>
       <main className={styles.main}>
         <Heading text="Σπουδές Προγραμματισμού" />
 
-        <div className={styles.fixed}>
-          <section className={styles.education} id="education">
+        <section className={styles.education} id="education">
+          <div className={styles['education-container']}>
             <TimelineItems />
-          </section>
-        </div>
+          </div>
+        </section>
       </main>
     </>
   );
-}
+};
+
+export default Education;
