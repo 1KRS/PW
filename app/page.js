13ecadf2816e4b65d7afe67f65b67c55539c65ea
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-11-15 17:36:00 +0100

    Update Landing Page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
-export default function Home({children}) {
+import LandingMain from '@/components/Backgrounds/LandingMain';
+import Link from 'next/link';
+import styles from './page.module.css';
+import UnderConstructionRibbon from '@/components/UnderConstructionRibbon';
+// import Slider from '@/components/Slider';
+
+const Home = () => {
   return (
-    <>
-      {children}
-    </>
+    <LandingMain>
+      <UnderConstructionRibbon />
+      <div className={styles['name']}>Δημήτρης Μανωλόπουλος</div>
+      <Link href="/programming" className={styles['landing__action']}>
+        <button type="button" className={styles['landing__action-button']}>
+          npm start
+        </button>
+      </Link>
+      {/* <Slider /> */}
+    </LandingMain>
   );
-}
+};
+
+export default Home;
