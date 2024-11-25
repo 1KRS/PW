2346429ef29923c6349d6a39e285368ae1eb871f
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-11-25 21:36:15 +0100

    Update Not Found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,22 +1,23 @@
 import BinaryRain from '@/components/Backgrounds/BinaryRain';
 import styles from './not-found.module.css';
 import Link from 'next/link';
+import LanguagesContainer from '@/components/LanguagesContainer';
 
 const notFound = () => {
   return (
     <div className="not-found">
-      <BinaryRain rainColor="#ffbb00" />
+      <BinaryRain screenType="not-found" speed="80" />
       <div className={styles.container}>
         <h1>404</h1>
         <div className={styles.text}>
           <h2>Η σελίδα δεν βρέθηκε</h2>
           {/* <p>Παρακαλούμε επιστρέψτε στην αρχική σελίδα.</p> */}
 
-            <Link href="/programming" className={styles.btn}>
-              Επιστροφή στην αρχική σελίδα
-            </Link>
-
+          <Link href="/programming" className={styles.btn}>
+            Επιστροφή στην αρχική σελίδα
+          </Link>
         </div>
+        <LanguagesContainer />
       </div>
     </div>
   );
