5a6a853354c5ccc7da2bd4998b89f55ac5c43097
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-11-25 21:35:09 +0100

    Update layout.js
    Add page metadata

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
   title: 'ΔΜ',
   description: 'Ο Προσωπικός Μου Ιστότοπος',
+  keywords: ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'NextJS'],
+  creator: 'Δημήτρης Μανωλόπουλος',
+  publisher: 'Δημήτρης Μανωλόπουλος',
 };
 
 export default function RootLayout({ children }) {
