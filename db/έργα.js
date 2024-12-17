4814f290c899faf27eee32f2ff723881019e146d
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 13:04:17 +0100

    Create έργα.js

---
+++ db/έργα.js
@@ -0,0 +1,4 @@
+export const έργα = [
+  { id: 1, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
+ 
+];
