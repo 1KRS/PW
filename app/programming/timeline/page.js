58daabcc0ac01ea0ebca2d1ad8dfa3d45eeb4e74
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 13:07:43 +0100

    Update Programming Timeline page

--- app/programming/timeline/page.js
+++ app/programming/timeline/page.js
@@ -1,18 +1,19 @@
 import Heading from '@/components/Heading';
 import TimelineItems from '@/components/TimelineItems';
 import styles from './page.module.css';
+import ProgrammingTimelineSideNav from '@/components/Navigations/ProgrammingTimelineSideNav';
 
 const Timeline = () => {
   return (
     <>
       <main className={styles.main}>
-        <Heading text="Σπουδές Προγραμματισμού" />
-
-        <div className={styles.fixed}>
-          <section className={styles.timeline} id="timeline">
+        <Heading text="Χρονολόγιο" />
+        <ProgrammingTimelineSideNav />
+        <section className={styles.timeline} id="timeline">
+          <div className={styles['timeline-container']}>
             <TimelineItems />
-          </section>
-        </div>
+          </div>
+        </section>
       </main>
     </>
   );
