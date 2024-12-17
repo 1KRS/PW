5629b8a5dc99c39f127462c5f1bfa358e40ce35c
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:48:15 +0100

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -3,24 +3,24 @@ import styles from './not-found.module.css';
 import Link from 'next/link';
 import LanguagesContainer from '@/components/LanguagesContainer';
 
-const notFound = () => {
+const NotFound = () => {
   return (
-    <div className="not-found">
-      <BinaryRain screenType="not-found" speed="80" />
-      <div className={styles.container}>
-        <h1>404</h1>
-        <div className={styles.text}>
-          <h2>Η σελίδα δεν βρέθηκε</h2>
-          {/* <p>Παρακαλούμε επιστρέψτε στην αρχική σελίδα.</p> */}
+    <div className={styles["not-found"]}>
+    <BinaryRain screenType="not-found" fontSize="14" speed="60" />
+    <div className={styles.container}>
+      <h1>404</h1>
+      <div className={styles.text}>
+        <h2>Η σελίδα δεν βρέθηκε</h2>
+        {/* <p>Παρακαλούμε επιστρέψτε στην αρχική σελίδα.</p> */}
 
-          <Link href="/programming" className={styles.btn}>
-            Επιστροφή στην αρχική σελίδα
-          </Link>
-        </div>
-        <LanguagesContainer />
+        <Link href="/programming" className={styles.btn}>
+          Επιστροφή στην αρχική σελίδα
+        </Link>
       </div>
+      <LanguagesContainer />
     </div>
+  </div>
   );
 };
 
-export default notFound;
+export default NotFound;
