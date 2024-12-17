b423aecc5336d2e0ed4e0c948011900950c2a769
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:49:53 +0100

    Update layout.js

--- app/programming/layout.js
+++ app/programming/layout.js
@@ -1,9 +1,17 @@
 import ProgrammingBackground from '@/components/Backgrounds/ProgrammingBackground';
-import MainHeader from '@/components/MainHeader';
-import ProgrammingSideNav from '@/components/ProgrammingSideNav';
-import MainFooter from '@/components/MainFooter';
+import MainHeader from '@/components/Header-Footer/MainHeader';
+import ProgrammingSideNav from '@/components/Navigations/ProgrammingSideNav';
+import MainFooter from '@/components/Header-Footer/MainFooter';
 // import MainSubheader from '@/components/MainSubheader';
-import UnderConstructionRibbon from '@/components/UnderConstructionRibbon';
+import UnderConstructionRibbon from '@/components/UnderDevelopmentRibbon';
+
+export const metadata = {
+  title: 'ΔΜ',
+  description: 'Ο Προσωπικός Μου Ιστότοπος ως προγραμματιστής',
+  keywords: ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'NextJS'],
+  creator: 'Δημήτρης Μανωλόπουλος',
+  publisher: 'Δημήτρης Μανωλόπουλος',
+};
 
 const ProgrammingLayout = ({ children }) => {
   return (
