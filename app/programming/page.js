2556c50f0b5543351719ae57e2e9e93ce7124c26
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:50:14 +0100

    Update Home page

--- app/programming/page.js
+++ app/programming/page.js
@@ -1,51 +1,16 @@
-// import ProgrammingBackground from '@/components/Backgrounds/ProgrammingBackground';
-import SocialIcons from '@/components/SocialIcons';
+// import SocialIcons from '@/components/SocialIcons';
 import styles from './page.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
-import Slider from '@/components/Slider';
+import Hero from '@/components/Hero';
 
-export default function Home() {
+
+const Home = () => {
   return (
     <>
       <main className={styles.main}>
-        <section className={styles.home} id="home">
-          <div className={styles['home-content']}>
-            <h1>
-              Χαίρεται, <span>Δημήτρης</span> εδώ
-            </h1>
-            <h3 className={styles['text-animation']}>
-              Είμαι <span></span>
-            </h3>
-            <p>
-              Ένας νεαρός Πλήρους-Εύρους Προγραμματιστής Ιστού με προσήλωση στην
-              εξέλιξη και στόχο την βέλτιστη λύση.
-            </p>
-
-            {/* <SocialIcons /> */}
-            <Slider />
-
-            <div className={styles['btn-group']}>
-              <Link href="#" className={styles.btn}>
-                Πρόσληψη
-              </Link>
-              <Link href="/contact" className={styles['btn-sec']}>
-                Επικοινωνία
-              </Link>
-            </div>
-            {/* <Slider /> */}
-          </div>
-          <div className={styles['home-img']}>
-            <Image
-              className={styles.img}
-              src="/icons/Φτερά Ικάρου (Λευκά Χωρίς Υπόβαθρο).png"
-              width={400}
-              height={400}
-              alt="Φτερά Ικάρου"
-            />
-          </div>
-        </section>
+        <Hero />
       </main>
     </>
   );
-}
+};
+
+export default Home;
