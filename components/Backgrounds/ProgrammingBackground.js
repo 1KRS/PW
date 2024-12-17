b8fa6a3d0bc70abd96cb34189a56fca5470bdcce
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 13:05:09 +0100

    Update Programming Background component

--- components/Backgrounds/ProgrammingBackground.js
+++ components/Backgrounds/ProgrammingBackground.js
@@ -1,6 +1,19 @@
+'use client'
+
+import { useAppContext } from '@/context/AppContext';
+import BinaryRain from './BinaryRain';
 import styles from './ProgrammingBackground.module.css';
 
-const ProgrammingBackground = ({ children }) => {
-  return <div className={styles["background-image"]}>{children}</div>;
+const ProgrammingBackground = ({screenType, fontSize, speed, children }) => {
+  const { programmingBackground } = useAppContext();
+
+  return programmingBackground === 'dynamic' ? (
+    <>
+    <BinaryRain screenType={screenType || "normal"} fontSize={fontSize || '14'} speed={speed || '80'}/>
+    {children}
+    </>
+  ) : (
+    <div className={styles['background-image']}>{children}</div>
+  );
 };
 export default ProgrammingBackground;
