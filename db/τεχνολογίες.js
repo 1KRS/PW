efb711cb87bb16911c4017907664209d7b83df86
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 13:03:15 +0100

    Create τεχνολογίες.js

---
+++ db/τεχνολογίες.js
@@ -0,0 +1,10 @@
+export const γλώσσες = ['HTML', 'CSS', 'Javascript', 'EJS'];
+export const βιβλιοθήκες = ['ReactJS'];
+export const πλαίσια = ['NextJS', 'React Native', 'Express'];
+export const περιβάλλοντα = ['NodeJS'];
+export const προγράμματα = ['MongoDB', ];
+export const συστήματα = ['PostGre SQL', 'GIT'];
+export const πλατφόρμες = ['GitHub'];
+export const διαχειριστέςΠακέτων = ['NPM']
+export const πακέτα = ['Luxon', 'Normalize', 'Styled Components']
+
