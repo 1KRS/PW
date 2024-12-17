d4b047295c5bf872358001b1c430e6dce48a59fb
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:58:23 +0100

    Create Todo Items component

---
+++ components/TodoItems.js
@@ -0,0 +1,41 @@
+import styles from './TodoItems.module.css';
+import { DateTime, Duration } from "luxon";
+import { προςΥλοποίηση } from '@/db/προςΥλοποίηση'
+
+
+
+const TodoItems = () => {
+
+  return (
+    <>
+      <div className={styles['todo-items']}>
+        {
+          προςΥλοποίηση.filter(todo => todo.ολοκλήρωση === false).map((todo, index) => {
+          const { ταυτότητα, τομέας, μέρος, τύπος, επεξήγηση, κρίσιμο, ενΕξελίξει, ολοκλήρωση } = todo;
+          
+          const μορφήΠεριβλήματος = ολοκλήρωση === true ? 'ολοκληρωμένο' : ενΕξελίξει ? 'ενΕξελίξει' : 'ενΑναμονή'
+          const κείμενο =
+            τύπος === 'Σελίδα' && επεξήγηση === 'Δημιουργία σελίδας'
+              ? `${επεξήγηση} «${μέρος}»`
+              : επεξήγηση;
+
+          return (
+            <article key={ταυτότητα} className={styles['todo-item']}>
+              <div className={styles['todo-content'] + ' ' + styles[`${μορφήΠεριβλήματος}`]}>
+              <div className={styles['todo-αριθμός']}>{index + 1}</div>
+                <h4 className={styles['επάνω-γραμμή']}>
+                  <div>{τύπος}</div>
+                  <div>για</div>
+                  <div> {μέρος}</div>
+              </h4>
+                <h3 className={styles['κάτω-γραμμή']}>{κείμενο}</h3>
+              </div>
+            </article>
+          );
+        })}
+
+      </div>
+    </>
+  );
+};
+export default TodoItems;
