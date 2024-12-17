9f47bdcde1bbcd8b90907fe5bb35c0eb7b2f0f19
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:58:53 +0100

    Create Background Toggle component

---
+++ components/ToggleButtons/BackgroundToggle.js
@@ -0,0 +1,42 @@
+'use client';
+
+import { μετάφραση } from '@/utils/μετάφραση';
+import styles from './BackgroundToggle.module.css';
+import { useAppContext } from '@/context/AppContext';
+import Input from '../Inputs/Input';
+
+const BackgroundToggle = () => {
+  const { language, programmingBackground, changeBackground } = useAppContext();
+
+  let checked = '';
+  let unchecked = '';
+
+  let backgroundType =
+    programmingBackground === 'static' ? 'dynamic' : 'static';
+
+  programmingBackground === 'static'
+    ? (checked = 'Δυναμικό')
+    : (checked = 'Στατικό');
+  programmingBackground === 'static'
+    ? (unchecked = 'Στατικό')
+    : (unchecked = 'Δυναμικό');
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        className={styles.toggle}
+        id="rounded"
+        onClick={() => changeBackground(backgroundType)}
+      />
+      <label
+        for="rounded"
+        className={styles.rounded}
+        // data-checked={μετάφραση(checked, language)}
+        // data-unchecked={μετάφραση(unchecked, language)}
+      ></label>
+    </div>
+  );
+};
+
+export default BackgroundToggle;
