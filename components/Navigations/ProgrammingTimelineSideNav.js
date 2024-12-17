cca57b8cbcd6aa300c2326c0216736683ca825f8
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-17 12:56:00 +0100

    Create Timeline Sidenav component

---
+++ components/Navigations/ProgrammingTimelineSideNav.js
@@ -0,0 +1,37 @@
+import styles from './ProgrammingTimelineSideNav.module.css';
+import { FaMinus } from 'react-icons/fa6';
+import { TbPoint } from 'react-icons/tb';
+
+const ProgrammingTimelineSideNav = () => {
+  return (
+    <nav className={styles['timeline-side-nav']}>
+      <ul className={styles['timeline-side-nav__items']}>
+        <li className={styles['timeline-side-nav__item']}>
+          <TbPoint className={styles['χρώμα-εικονιδίου-ολοκληρωμένο']} />{' '}
+          <div className={styles['κείμενο']}>Ολοκληρωμένα</div>
+        </li>
+        <li className={styles['timeline-side-nav__item']}>
+          <TbPoint className={styles['χρώμα-εικονιδίου-εν-εξελίξει']} />{' '}
+          <div className={styles['κείμενο']}>Σε εξέλιξη</div>
+        </li>
+        <li className={styles['timeline-side-nav__item']}>
+          <TbPoint className={styles['χρώμα-εικονιδίου-εν-αναμονή']} />{' '}
+          <div className={styles['κείμενο']}>Εν αναμονή</div>
+        </li>
+        <li className={styles['timeline-side-nav__item']}>
+          <FaMinus className={styles['χρώμα-εικονιδίου-σπουδές']} />{' '}
+          <div className={styles['κείμενο']}>Σπουδές</div>
+        </li>
+        <li className={styles['timeline-side-nav__item']}>
+          <FaMinus className={styles['χρώμα-εικονιδίου-εργασία']} />{' '}
+          <div className={styles['κείμενο']}>Εργασία</div>
+        </li>
+        <li className={styles['timeline-side-nav__item']}>
+          <FaMinus className={styles['χρώμα-εικονιδίου-έργα']} />{' '}
+          <div className={styles['κείμενο']}>Έργα</div>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default ProgrammingTimelineSideNav;
