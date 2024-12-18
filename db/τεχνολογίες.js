5ab086f5ef9e58140b14a63a0a8d6a0477664ee7
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-18 22:41:02 +0100

    Update τεχνολογίες.js

--- db/τεχνολογίες.js
+++ db/τεχνολογίες.js
@@ -1,10 +1,10 @@
 export const γλώσσες = ['HTML', 'CSS', 'Javascript', 'EJS'];
-export const βιβλιοθήκες = ['ReactJS'];
+export const βιβλιοθήκες = ['ReactJS','Bootstrap'];
 export const πλαίσια = ['NextJS', 'React Native', 'Express'];
 export const περιβάλλοντα = ['NodeJS'];
 export const προγράμματα = ['MongoDB', ];
 export const συστήματα = ['PostGre SQL', 'GIT'];
 export const πλατφόρμες = ['GitHub'];
 export const διαχειριστέςΠακέτων = ['NPM']
-export const πακέτα = ['Luxon', 'Normalize', 'Styled Components', 'React Circle Flags']
+export const πακέτα = [ 'Luxon', 'Normalize', 'Styled Components', 'React Circle Flags' ]
 
