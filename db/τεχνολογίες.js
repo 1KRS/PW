44d2986b9a3ca1db697609a49309c4ca157a9dba
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-19 23:38:50 +0100

    Update τεχνολογίες.js

--- db/τεχνολογίες.js
+++ db/τεχνολογίες.js
@@ -6,5 +6,5 @@ export const προγράμματα = ['MongoDB', ];
 export const συστήματα = ['PostGre SQL', 'GIT'];
 export const πλατφόρμες = ['GitHub'];
 export const διαχειριστέςΠακέτων = ['NPM']
-export const πακέτα = [ 'Luxon', 'Normalize', 'Styled Components', 'React Circle Flags' ]
+export const πακέτα = [ 'Axios', 'Luxon', 'Normalize', 'Styled Components', 'React Circle Flags', 'JavaScript Cookie' ]
 
