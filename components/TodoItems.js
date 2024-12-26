8d06feee84d006e99cd3174fccc919dbf8604043
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2024-12-26 02:50:14 +0100

    Update TodoItems.js

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -2,6 +2,8 @@ import styles from './TodoItems.module.css';
 import { DateTime, Duration } from "luxon";
 import { προςΥλοποίηση } from '@/db/προςΥλοποίηση'
 
+import TranslatedText from './TranslatedText';
+import { μετάφραση } from '@/utils/μετάφραση';
 
 
 const TodoItems = () => {
@@ -24,11 +26,14 @@ const TodoItems = () => {
               <div className={styles['todo-content'] + ' ' + styles[`${μορφήΠεριβλήματος}`]}>
               <div className={styles['todo-αριθμός']}>{index + 1}</div>
                 <h4 className={styles['επάνω-γραμμή']}>
-                  <div>{τύπος}</div>
-                  <div>για</div>
-                  <div> {μέρος === 'Ιστότοπος' ? 'Ιστότοπο' : μέρος}</div>
+                  <TranslatedText>{τύπος}</TranslatedText>
+                  {' '}
+                  <TranslatedText>για</TranslatedText>
+                  {' '}
+                  <TranslatedText>{μέρος === 'Ιστότοπος' ? 'Ιστότοπο' : μέρος}</TranslatedText>
+                  
               </h4>
-                <h3 className={styles['κάτω-γραμμή']}>{κείμενο}</h3>
+                <h3 className={styles['κάτω-γραμμή']}><TranslatedText>{κείμενο}</TranslatedText></h3>
               </div>
             </article>
           );
