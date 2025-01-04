e5cbcc2e73778380b1f382d7991ffe30ae0abf13
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-04 02:52:16 +0100

    Create ΤελείαΓεγονότος.js

---
+++ components/Event/ΤελείαΓεγονότος.js
@@ -0,0 +1,8 @@
+import styles from '@/components/TimelineItems.module.css';
+
+const ΤελείαΓεγονότος = () => {
+
+  return <div className={styles['timeline-dot']} />;
+};
+
+export default ΤελείαΓεγονότος;
