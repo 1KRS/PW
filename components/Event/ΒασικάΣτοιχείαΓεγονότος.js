8f2c80428e7a54add5c4fbff0ac652269f4b994a
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-08 15:38:53 +0100

    Update ΒασικάΣτοιχείαΓεγονότος.js

--- components/Event/ΒασικάΣτοιχείαΓεγονότος.js
+++ components/Event/ΒασικάΣτοιχείαΓεγονότος.js
@@ -1,14 +1,16 @@
 import styles from '@/components/TimelineItems.module.css';
 
 const ΒασικάΣτοιχείαΓεγονότος = ({
-  μονός,
+  στιγμή,
   μεΉΧωρίςΔευτερεύουσαΓραμμή,
   children,
 }) => {
+  const { ταυτότητα } = στιγμή;
 
-  const μονόςΖυγόςΔευτερεύουσαΓραμμή = μονός
-  ? 'event-odd'
-  : 'event-even';
+  const μονόςΑριθμός = ταυτότητα % 2 !== 0 ? true : false;
+
+  const μονόςΖυγόςΔευτερεύουσαΓραμμή = μονόςΑριθμός ? 'event-odd' : 'event-even';
+
   return (
     <div
       className={
