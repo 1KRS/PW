2816464a9af72de89b46418fb3431ff874efea03
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-09 02:27:59 +0100

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -2,24 +2,25 @@ import BinaryRain from '@/components/Backgrounds/BinaryRain';
 import styles from './not-found.module.css';
 import Link from 'next/link';
 import LanguagesContainer from '@/components/LanguagesContainer';
+import TranslatedText from '@/components/TranslatedText';
 
 const NotFound = () => {
   return (
-    <div className={styles["not-found"]}>
-    <BinaryRain screenType="not-found" fontSize="14" speed="60" />
-    <div className={styles.container}>
-      <h1>404</h1>
-      <div className={styles.text}>
-        <h2>Η σελίδα δεν βρέθηκε</h2>
-        {/* <p>Παρακαλούμε επιστρέψτε στην αρχική σελίδα.</p> */}
-
-        <Link href="/programming" className={styles.btn}>
-          Επιστροφή στην αρχική σελίδα
-        </Link>
+    <div className={styles['not-found']}>
+      <BinaryRain screenType="not-found" fontSize="14" speed="60" />
+      <div className={styles.container}>
+        <h1>404</h1>
+        <div className={styles.text}>
+          <h2>
+            <TranslatedText>Η σελίδα δεν βρέθηκε</TranslatedText>
+          </h2>
+          <Link href="/programming" className={styles.btn}>
+            <TranslatedText>Επιστροφή στην αρχική σελίδα</TranslatedText>
+          </Link>
+        </div>
+        <LanguagesContainer />
       </div>
-      <LanguagesContainer />
     </div>
-  </div>
   );
 };
 
