8d767c6ca01fa5ce6243b483b07dc76eb60ca7ef
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-09 02:27:53 +0100

    Update error.js

--- app/error.js
+++ app/error.js
@@ -4,24 +4,26 @@ import BinaryRain from '@/components/Backgrounds/BinaryRain';
 import styles from './error.module.css';
 import Link from 'next/link';
 import LanguagesContainer from '@/components/LanguagesContainer';
+import TranslatedText from '@/components/TranslatedText';
 
 const Error = () => {
-  return ( <div className={styles['error']}>
+  return (
+    <div className={styles['error']}>
       <BinaryRain screenType="error" fontSize="14" speed="60" />
       <div className={styles.container}>
         <h1>500</h1>
         <div className={styles.text}>
-          <h2>Κάποιο λάθος προέκυψε.</h2>
-          {/* <p>Παρακαλούμε επιστρέψτε στην αρχική σελίδα.</p> */}
-
+          <h2>
+            <TranslatedText>Κάποιο λάθος προέκυψε</TranslatedText>
+          </h2>
           <Link href="/programming" className={styles.btn}>
-            Επιστροφή στην αρχική σελίδα
+            <TranslatedText>Επιστροφή στην αρχική σελίδα</TranslatedText>
           </Link>
         </div>
         <LanguagesContainer />
       </div>
     </div>
-)
-}
+  );
+};
 
 export default Error;
