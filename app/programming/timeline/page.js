370804bf60a27f29ea783248fcb0623b0ae2f73c
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-14 17:32:29 +0100

    Update page.js

--- app/programming/timeline/page.js
+++ app/programming/timeline/page.js
@@ -1,20 +1,23 @@
+import styles from './page.module.css';
+
 import Heading from '@/components/Heading';
 import TimelineItems from '@/components/TimelineItems';
-import styles from './page.module.css';
 import ProgrammingTimelineSideNav from '@/components/Navigations/ProgrammingTimelineSideNav';
 
 const Timeline = () => {
   return (
-    <main className={styles.main}>
-      <Heading text="Χρονολόγιο" />
-      <ProgrammingTimelineSideNav σελίδα="Χρονολόγιο" />
+    <>
+      <main className={styles.main}>
+        <Heading text="Χρονολόγιο" />
+        <ProgrammingTimelineSideNav σελίδα="Χρονολόγιο" />
 
-      <section className={styles.timeline} id="timeline">
-        <div className={styles['timeline-container']}>
-          <TimelineItems />
-        </div>
-      </section>
-    </main>
+        <section className={styles.timeline} id="timeline">
+          <div className={styles['timeline-container']}>
+            <TimelineItems />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
