9903da3543c57a6cf43232a0991e0eb81309aea3
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-19 18:08:45 +0100

    Create ΣτοιχείοΠλαϊνήςΣτήλης.js

---
+++ components/Πλήκτρα/ΣτοιχείοΠλαϊνήςΣτήλης.js
@@ -0,0 +1,54 @@
+'use client';
+
+import styles from '@/components/Navigations/ProgrammingTimelineSideNav.module.css';
+
+import TranslatedText from '@/components/TranslatedText';
+
+import { FaMinus } from 'react-icons/fa6';
+import { TiMinusOutline } from 'react-icons/ti';
+import { RiAlibabaCloudLine } from 'react-icons/ri';
+
+import { useAppContext } from '@/context/AppContext';
+
+const ΣτοιχείοΠλαϊνήςΣτήλης = ({ είδοςΚατηγορίας }) => {
+  const { φίλτροΚατάστασηςΓεγονότωνΧρονολογίου, changeEventFilter } =
+    useAppContext();
+
+  const handleClickEvent = () => {
+    φίλτροΚατάστασηςΓεγονότωνΧρονολογίου === είδοςΚατηγορίας
+      ? changeEventFilter('')
+      : changeEventFilter(είδοςΚατηγορίας);
+  };
+
+  const επιλεγμένοΉΜη =
+    φίλτροΚατάστασηςΓεγονότωνΧρονολογίου === είδοςΚατηγορίας
+      ? 'επιλεγμένο'
+      : '';
+
+  const χρώμαΕικονιδίου = `χρώμα-εικονιδίου-${είδοςΚατηγορίας
+    .toLowerCase()
+    .replace(' ', '-')}`;
+
+  return (
+    <li
+      className={styles['στοιχείο'] + ' ' + styles[`${επιλεγμένοΉΜη}`]}
+      onClick={() => handleClickEvent()}
+    >
+      {φίλτροΚατάστασηςΓεγονότωνΧρονολογίου === είδοςΚατηγορίας ? (
+        <RiAlibabaCloudLine className={styles[`${χρώμαΕικονιδίου}`]} />
+      ) : (είδοςΚατηγορίας === 'Εν αναμονή' ||
+          είδοςΚατηγορίας === 'Εν εξελίξει' ||
+          είδοςΚατηγορίας === 'Ολοκληρωμένο' ||
+          είδοςΚατηγορίας === 'Παρατημένο') ? (
+          <TiMinusOutline className={styles[`${χρώμαΕικονιδίου}`]} />
+        ) : (
+        <FaMinus className={styles[`${χρώμαΕικονιδίου}`]} />
+      )}
+      <div className={styles['κείμενο']}>
+        <TranslatedText>{είδοςΚατηγορίας}</TranslatedText>
+      </div>
+    </li>
+  );
+};
+
+export default ΣτοιχείοΠλαϊνήςΣτήλης;
