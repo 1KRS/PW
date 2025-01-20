1c26a00bc3e0e1fb9a399efbf2cfe7213da59a2c
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-01-20 12:27:20 +0100

    Update page.js

--- app/programming/timeline/page.js
+++ app/programming/timeline/page.js
@@ -4,13 +4,15 @@ import Heading from '@/components/Heading';
 import ΣτοιχείαΧρονολογίου from '@/components/Χρονολόγιο/ΣτοιχείαΧρονολογίου';
 import ProgrammingTimelineSideNav from '@/components/Navigations/ProgrammingTimelineSideNav';
 
-
 const Timeline = () => {
   return (
     <>
       <main className={styles.main}>
         <Heading text="Χρονολόγιο" />
-        <ProgrammingTimelineSideNav σελίδα="Χρονολόγιο" />
+        <ProgrammingTimelineSideNav
+          σελίδα="Χρονολόγιο"
+          ύπαρξηΦίλτρων="υπαρκτά"
+        />
 
         <section className={styles.timeline} id="timeline">
           <div className={styles['timeline-container']}>
