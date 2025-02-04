8935753e5afcf5e817d8f630250e50297a8f116d
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-04 22:05:17 +0100

    Update page.js

--- app/programming/portfolio/page.js
+++ app/programming/portfolio/page.js
@@ -3,11 +3,11 @@ import styles from './page.module.css';
 import Image from 'next/image';
 import { FaRegHandPointer } from 'react-icons/fa6';
 import Link from 'next/link';
-import { έργα } from '@/db/έργα'
+import { έργα } from '@/db/έργα';
 
 export const metadata = {
   title: 'Το Χαρτοφυλάκιό μου',
-  description: 'Ο Προσωπικός Μου Ιστότοπος ως προγραμματιστής',
+  description: 'Ο προσωπικός μου ιστότοπος ως προγραμματιστής',
   keywords: ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'NextJS'],
   creator: 'Δημήτρης Μανωλόπουλος',
   publisher: 'Δημήτρης Μανωλόπουλος',
@@ -15,7 +15,7 @@ export const metadata = {
 
 // const projects = [
 //     { id: 1, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
-   
+
 //   ];
 
 const Portfolio = () => {
@@ -26,31 +26,26 @@ const Portfolio = () => {
 
         <section className={styles.portfolio} id="portfolio">
           <div className={styles['portfolio-container']}>
-
-
-{έργα.map((project) => {
-        return (
-          <div key={project.id} className={styles['portfolio-item']}>
-              <Image
-                className={styles.img}
-                src={project.imageSrc}
-                fill
-                alt={project.imageAlt}
-              />
-              <Link
-                href={project.link}
-                className={styles['portfolio-layer']}
-              >
-                <h4>{project.title}</h4>
-                <p>
-                  {project.text}
-                </p>
-                <FaRegHandPointer className={styles['link-icon']} />
-              </Link>
-            </div>
-        )
-      })}
-
+            {έργα.map((project) => {
+              return (
+                <div key={project.id} className={styles['portfolio-item']}>
+                  <Image
+                    className={styles.img}
+                    src={project.imageSrc}
+                    fill
+                    alt={project.imageAlt}
+                  />
+                  <Link
+                    href={project.link}
+                    className={styles['portfolio-layer']}
+                  >
+                    <h4>{project.title}</h4>
+                    <p>{project.text}</p>
+                    <FaRegHandPointer className={styles['link-icon']} />
+                  </Link>
+                </div>
+              );
+            })}
 
             {/* <div className={styles['portfolio-item']}>
               <Image
@@ -185,8 +180,6 @@ const Portfolio = () => {
                 <FaRegHandPointer className={styles['link-icon']} />
               </Link>
             </div> */}
-
-
           </div>
         </section>
       </main>
