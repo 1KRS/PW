845a6e2c5fbbbcb8cbf9d81635d91fb5586aeb0a
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-04 22:05:37 +0100

    Update page.js

--- app/programming/timeline/page.js
+++ app/programming/timeline/page.js
@@ -4,6 +4,14 @@ import Heading from '@/components/Heading';
 import ΣτοιχείαΧρονολογίου from '@/components/Χρονολόγιο/ΣτοιχείαΧρονολογίου';
 import ProgrammingTimelineSideNav from '@/components/Navigations/ProgrammingTimelineSideNav';
 
+export const metadata = {
+  title: 'Το Χρονολόγιό Μου',
+  description: 'Η πορεία μου ως προγραμματιστής',
+  keywords: ['χρονολόγιο', 'timeline', 'tidlinje'],
+  creator: 'Δημήτρης Μανωλόπουλος',
+  publisher: 'Δημήτρης Μανωλόπουλος',
+};
+
 const Timeline = () => {
   return (
     <>
