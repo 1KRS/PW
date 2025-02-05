ad0a6a8745e1a23d487b0bd9c470f42d4b531ab6
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-05 23:18:14 +0100

    Update έργα.js

--- db/έργα.js
+++ db/έργα.js
@@ -1,4 +1,10 @@
 export const έργα = [
   { id: 1, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
+  { id: 2, title: 'Το Παίγνιο Του Σάιμον', text: 'Το γνωστό σε όλους μας παίγνιο μνήμης.', link:'https://1krs.github.io/simon-game/' , imageSrc: '/images/Το Παίγνιο Του Σάιμον.png', imageAlt: 'Παίγνιο Μνήμης'},
+  { id: 3, title: 'Τύμπανα & Κρουστά', text: 'Ένας ιστότοπος που μπορείς να παίξεις τύμπανα και κρουστά.', link:'https://1krs.github.io/online-drumkit/' , imageSrc: '/images/Τύμπανα & Κρουστά.png', imageAlt: 'Ιστότοπος με τύμπανα και κρουστά'},
+  { id: 4, title: 'Τρίλιζα', text: 'Το γνωστό σε όλους μας παίγνιο στρατηγικής.', link:'https://1krs.github.io/Tic_Tac_Toe/' , imageSrc: '/images/Τρίλιζα.png', imageAlt: 'Παίγνιο Στρατηγικής'},
+  { id: 5, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
+  { id: 6, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
+  { id: 7, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
  
 ];
