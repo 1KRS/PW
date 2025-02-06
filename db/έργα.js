e699950d5238da34340343720f096e81c6aeb767
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-06 20:23:57 +0100

    Update έργα.js

--- db/έργα.js
+++ db/έργα.js
@@ -5,6 +5,6 @@ export const έργα = [
   { id: 4, title: 'Τρίλιζα', text: 'Το γνωστό σε όλους μας παίγνιο στρατηγικής.', link:'https://1krs.github.io/Tic_Tac_Toe/' , imageSrc: '/images/Τρίλιζα.png', imageAlt: 'Παίγνιο Στρατηγικής'},
   { id: 5, title: 'Υπολογιστής Επένδυσης', text: 'Υπολόγισε την απόδοση της επένδυσής σου με αυτό το διαδικτυακό εργαλείο.', link:'https://1krs.github.io/Investment_Calculator/' , imageSrc: '/images/Υπολογιστής Επένδυσης.png', imageAlt: 'Φωτογραφία Υπολογιστή Επένδυσης'},
   { id: 6, title: 'Εσωτερικό Ωρολόι', text: 'Εκπαίδευσε το εσωτερικό σου ωρολόι για να συγχρονιστεί με τον πραγματικό χρόνο.', link:'https://1krs.github.io/Internal_Clock/' , imageSrc: '/images/Εσωτερικό Ωρολόι.png', imageAlt: 'Φωτογραφία Ιστοτόπου'},
-  { id: 7, title: 'Εργασιομανία', text: 'Οργάνωσε το κυνήγι εργασίας σου με αυτήν την εφαρμογή ιχνηλάτησης.', link:'http://ergasiomania.onrender.com' , imageSrc: '/images/Εργασιομανία.png', imageAlt: 'Ιστότοπος Εργασιομανίας'},
+  { id: 7, title: 'Προσωπικός Ιστότοπος', text: 'Η τρίτη προσπάθεια ανάπτυξης του προσωπικού μου ιστοτόπου και η πρώτη ολοκληρωμένη.', link:'https://dm-lilac.vercel.app/programming/portfolio' , imageSrc: '/images/Προσωπικός Ιστότοπος.png', imageAlt: 'Προσωπικός Ιστότοπος'},
  
 ];
