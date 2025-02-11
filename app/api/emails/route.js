04f249dea0a173db5f0f388da2f7c77eab64fc6d
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-11 15:12:59 +0100

    Create route.js

---
+++ app/api/emails/route.js
@@ -0,0 +1,26 @@
+import ΜνμΑπόΕπισκέπτες from '@/app/emails/ΜνμΑπόΕπισκέπτες';
+
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request, res) {
+  const { fullName, email, number, subject, text } = await request.json();
+
+  try {
+    const { results, error } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'ik4r0s@icloud.com',
+      subject: 'Νέο μήνυμα από προσωπικό ιστότοπο',
+      react: ΜνμΑπόΕπισκέπτες({ fullName, email, number, subject, text }),
+    });
+
+    if (error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+
+    return Response.json({ data: results }, { status: 201 });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
