81dce1c8d6e754a21e9cfab9c728c0b09c4780d2
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-12 11:38:34 +0100

    Create ΕικονίδιαΔευτερεύουσαςΓραμμής.js

---
+++ components/Χρονολόγιο/ΕικονίδιαΔευτερεύουσαςΓραμμής.js
@@ -0,0 +1,35 @@
+'use client';
+
+import styles from '@/components/Χρονολόγιο/ΣτοιχείαΧρονολογίου.module.css';
+
+import { RiHomeOfficeLine } from 'react-icons/ri';
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2';
+import { FaSchoolFlag } from 'react-icons/fa6';
+
+import { useAppContext } from '@/context/AppContext';
+import { μετάφραση } from '@/utils/μετάφραση';
+
+const ΕικονίδιαΔευτερεύουσαςΓραμμής = ({ είδος, τοποθεσία }) => {
+  const { language } = useAppContext();
+
+  return (
+    <div
+      className={styles['περιέκτης-εικονιδίων']}
+      data-types-icon={μετάφραση(τοποθεσία, language)}
+    >
+      {τοποθεσία === 'Απομακρυσμένα' ? (
+        <RiHomeOfficeLine
+          className={styles['εικονίδιο-δευτερεύουσας-γραμμής']}
+        />
+      ) : τοποθεσία === 'Δια ζώσης' && είδος === 'Εργασία' ? (
+        <HiOutlineBuildingOffice2
+          className={styles['εικονίδιο-δευτερεύουσας-γραμμής']}
+        />
+      ) : τοποθεσία === 'Δια ζώσης' && είδος === 'Σπουδές' ? (
+        <FaSchoolFlag className={styles['εικονίδιο-δευτερεύουσας-γραμμής']} />
+      ) : null}
+    </div>
+  );
+};
+
+export default ΕικονίδιαΔευτερεύουσαςΓραμμής;
