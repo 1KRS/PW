2970d6a28d59a9066588b9e4687e26cf4ab55da9
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-12 01:59:36 +0100

    Create BackgroundToggle2.js

---
+++ components/Πλήκτρα/ToggleButtons/BackgroundToggle2.js
@@ -0,0 +1,30 @@
+'use client';
+
+import styles from './BackgroundToggle2.module.css';
+import { useAppContext } from '@/context/AppContext';
+
+const BackgroundToggle2 = () => {
+  const { programmingBackground, changeBackground } = useAppContext();
+
+  // let checked = programmingBackground === 'static' ? 'Δυναμικό' : 'Στατικό';
+  // let unchecked = programmingBackground === 'static' ? 'Στατικό' : 'Δυναμικό';
+
+  let backgroundType =
+    programmingBackground === 'static' ? 'dynamic' : 'static';
+
+  return (
+    <div className={styles['container']}>
+      <label className={styles['toggle-box']}>
+        <input
+          type="checkbox"
+          className={styles['input']}
+          checked={backgroundType === 'static' ? true : false}
+          onChange={() => changeBackground(backgroundType)}
+        />
+        <div className={styles['button']}></div>
+      </label>
+    </div>
+  );
+};
+
+export default BackgroundToggle2;
