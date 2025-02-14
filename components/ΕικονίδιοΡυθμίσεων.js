1741f2925a1dcfe206017383b5a34767510264d4
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-14 22:13:56 +0100

    Create ΕικονίδιοΡυθμίσεων.js

---
+++ components/ΕικονίδιοΡυθμίσεων.js
@@ -0,0 +1,8 @@
+import styles from './ΕικονίδιοΡυθμίσεων.module.css';
+
+import { VscSettings } from 'react-icons/vsc';
+const ΕικονίδιοΡυθμίσεων = () => {
+  return <VscSettings className={styles['εικονίδιο-ρυθμίσεων']} />;
+};
+
+export default ΕικονίδιοΡυθμίσεων;
