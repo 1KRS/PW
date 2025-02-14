14afeadf37bee00f1c8b1b13706d0895f8f25191
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-14 22:12:41 +0100

    Update Υποσέλιδο.js

--- components/Κεφαλίδες-Υποσέλιδα/Υποσέλιδο.js
+++ components/Κεφαλίδες-Υποσέλιδα/Υποσέλιδο.js
@@ -1,31 +1,34 @@
-import Link from 'next/link';
 import styles from './Υποσέλιδο.module.css';
 
-import { στοιχείαΗμερομηνιών } from '@/utils/στοιχείαΗμερομηνιών';
+import Link from 'next/link';
 
 import SocialIcons from '../SocialIcons';
 import TranslatedText from '../TranslatedText';
+
+import { στοιχείαΗμερομηνιών } from '@/utils/στοιχείαΗμερομηνιών';
 
 const Υποσέλιδο = () => {
   const { τρέχονΈτος } = στοιχείαΗμερομηνιών();
 
   return (
     <footer className={styles.υποσέλιδο}>
-      <nav className={styles['home-bottom-nav']}>
-        <ul className={styles.στοιχεία}>
-          <Link href="/home" className={styles.στοιχείο}>
-            <TranslatedText>Συχνές Ερωτήσεις</TranslatedText>
-          </Link>
-          <SocialIcons />
-          <Link href="/home" className={styles.στοιχείο}>
-            <TranslatedText>Σχετικά με εμένα</TranslatedText>
-          </Link>
-        </ul>
-      </nav>
-      <div className={styles['copyright-container']}>
-        <TranslatedText>Δημήτρης Μανωλόπουλος</TranslatedText>
-        {` © ${τρέχονΈτος} `}
-        <TranslatedText>Επιφύλαξη Παντός Δικαιώματος</TranslatedText>
+      <div className={styles['περιέκτης-κεντρικού-τμήματος-υποσέλιδου']}>
+        <nav className={styles['home-bottom-nav']}>
+          <ul className={styles.στοιχεία}>
+            <Link href="/home" className={styles.στοιχείο}>
+              <TranslatedText>Συχνές Ερωτήσεις</TranslatedText>
+            </Link>
+            <SocialIcons />
+            <Link href="/home" className={styles.στοιχείο}>
+              <TranslatedText>Σχετικά με εμένα</TranslatedText>
+            </Link>
+          </ul>
+        </nav>
+        <div className={styles['copyright-container']}>
+          <TranslatedText>Δημήτρης Μανωλόπουλος</TranslatedText>
+          {` © ${τρέχονΈτος} `}
+          <TranslatedText>Επιφύλαξη Παντός Δικαιώματος</TranslatedText>
+        </div>
       </div>
     </footer>
   );
