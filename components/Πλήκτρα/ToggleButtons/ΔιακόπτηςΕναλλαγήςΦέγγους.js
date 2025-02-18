653ce5376cf789af59271fd00e4a73328b267ae1
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-18 01:17:25 +0100

    Create ΔιακόπτηςΕναλλαγήςΦέγγους.js

---
+++ components/Πλήκτρα/ToggleButtons/ΔιακόπτηςΕναλλαγήςΦέγγους.js
@@ -0,0 +1,32 @@
+'use client';
+
+import styles from './ΔιακόπτηςΕναλλαγήςΦέγγους.module.css';
+
+import { useAppContext } from '@/context/AppContext';
+
+const GlowToggleButton = () => {
+  const { εναλλαγήΦέγγουςΣτοιχείων, φέγγοςΣτοιχείων } = useAppContext();
+
+  const κατάστασηΠεριγράμματος =
+    φέγγοςΣτοιχείων === true ? 'ενεργό' : 'ανενεργό';
+
+  return (
+    <div
+      className={
+        styles['περιέκτης-διακόπτη'] + ' ' + styles[`${κατάστασηΠεριγράμματος}`]
+      }
+    >
+      <label className={styles['τετράγωνο-επιλογής']}>
+        <input
+          type="checkbox"
+          className={styles['input']}
+          checked={φέγγοςΣτοιχείων === true ? true : false}
+          onChange={() => εναλλαγήΦέγγουςΣτοιχείων(!φέγγοςΣτοιχείων)}
+        />
+        <div className={styles['διακόπτης']} />
+      </label>
+    </div>
+  );
+};
+
+export default GlowToggleButton;
