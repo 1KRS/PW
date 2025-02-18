e80cfc74eeedd468fe672559b5d591204c02dfe2
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-18 16:02:42 +0100

    Update Ηρώο.js

--- components/Ηρώο.js
+++ components/Ηρώο.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Slider from '@/components/Slider';
 import Slider2 from '@/components/Slider2';
 import TranslatedText from '@/components/TranslatedText';
+import ΠλαίσιοΜορφής from '@/components/ΠλαίσιοΜορφής';
 
 import { useAppContext } from '@/context/AppContext';
 
@@ -16,63 +17,61 @@ const Hero = () => {
   return (
     <section
       className={
-        styles[`${υπόβαθροΠρογραμματισμού === 'δυναμικό' ? 'αρχική' : 'αρχική'}`] //<-----
+        styles[
+          `${υπόβαθροΠρογραμματισμού === 'δυναμικό' ? 'αρχική' : 'αρχική'}`
+        ] //<-----
       }
       id="αρχική"
     >
-      <div
-        className={
-          styles['περιεχόμενο-αρχικής'] +
-          ' ' +
-          styles[
-            `${μορφήΣτοιχείων === 'υαλομορφισμός' ? 'με-υαλομορφισμό' : μορφήΣτοιχείων === 'ημιδιαφανή στοιχεία' ? 'με-ημιδιαφανή-στοιχεία' : μορφήΣτοιχείων === 'αδιαφανή στοιχεία' ? 'με-αδιαφανή-στοιχεία' : ''}`
-          ] +
-          ' ' +
-          styles[`${φέγγοςΣτοιχείων === true ? 'με-φέγγος-στοιχείων' : ''}`]
-        }
+      <ΠλαίσιοΜορφής
+        καθορισμόςΔιαστάσεων="από μέσα"
+        ακτίναΓωνίας={35}
+        ενεργόςΔιακόπτηςΦέγγους={true}
       >
-        {language === 'english' ? (
-          <h1>
-            Hi, it&apos;s<pre> </pre>
-            <span> Dimitris</span>
-          </h1>
-        ) : language === 'svenska' ? (
-          <h1>
-            Hej, jag heter<pre> </pre>
-            <span>Dimitris</span>
-          </h1>
-        ) : (
-          <h1>
-            Χαίρεται,<pre> </pre>
-            <span>Δημήτρης</span>
-            <pre> </pre>εδώ
-          </h1>
-        )}
+        <div className={styles['περιεχόμενο-αρχικής']}>
+          {language === 'english' ? (
+            <h1>
+              Hi, it&apos;s<pre> </pre>
+              <span> Dimitris</span>
+            </h1>
+          ) : language === 'svenska' ? (
+            <h1>
+              Hej, jag heter<pre> </pre>
+              <span>Dimitris</span>
+            </h1>
+          ) : (
+            <h1>
+              Χαίρεται,<pre> </pre>
+              <span>Δημήτρης</span>
+              <pre> </pre>εδώ
+            </h1>
+          )}
 
-        <h3>
-          <TranslatedText>
-            Είμαι ένας νεαρός Πλήρους-Εύρους Προγραμματιστής Ιστού με προσήλωση
-            στην εξέλιξη και στόχο την βέλτιστη λύση.
-          </TranslatedText>
-        </h3>
+          <h3>
+            <TranslatedText>
+              Είμαι ένας νεαρός Πλήρους-Εύρους Προγραμματιστής Ιστού με
+              προσήλωση στην εξέλιξη και στόχο την βέλτιστη λύση.
+            </TranslatedText>
+          </h3>
 
-        <div className={styles['περιέκτης-ταινιών-κύλισης']}>
-          <Slider />
-          <Slider2 />
+          <div className={styles['περιέκτης-ταινιών-κύλισης']}>
+            <Slider />
+            <Slider2 />
+          </div>
+
+          <div className={styles['περιέκτης-πλήκτρων']}>
+            <Link href="#" className={styles['βασικό-πλήκτρο']}>
+              <TranslatedText>Πρόσληψη</TranslatedText>
+            </Link>
+            <Link
+              href="/programming/contact"
+              className={styles['δευτερεύον-πλήκτρο']}
+            >
+              <TranslatedText>Επικοινωνία</TranslatedText>
+            </Link>
+          </div>
         </div>
-
-        <div className={styles['περιέκτης-πλήκτρων']}>
-          <Link href="#" className={styles['βασικό-πλήκτρο']}>
-            <TranslatedText>Πρόσληψη</TranslatedText>
-          </Link>
-          <Link
-            href="/programming/contact"
-            className={styles['δευτερεύον-πλήκτρο']}
-          >
-            <TranslatedText>Επικοινωνία</TranslatedText>
-          </Link>
-        </div>
-      </div>
+      </ΠλαίσιοΜορφής>
       <div className={styles['εικόνα-αρχικής']}>
         <Image
           className={styles.εικόνα}
