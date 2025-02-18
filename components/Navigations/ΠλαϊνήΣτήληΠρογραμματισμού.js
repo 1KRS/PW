8499aef2e67b1a4554fc526a067363078c656c67
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-18 16:02:25 +0100

    Create ΠλαϊνήΣτήληΠρογραμματισμού.js

---
+++ components/Navigations/ΠλαϊνήΣτήληΠρογραμματισμού.js
@@ -0,0 +1,57 @@
+import styles from './ΠλαϊνήΣτήληΠρογραμματισμού.module.css';
+
+import NavLinkThatStartsWith from '@/components/NavLinks/NavLinkThatStartsWith';
+import NavLinkThatEndsWith from '@/components/NavLinks/NavLinkThatEndsWith';
+import ΠλαίσιοΜορφής from '@/components/ΠλαίσιοΜορφής';
+
+// import { MdOutlineStarRate } from 'react-icons/md';
+import { IoMdCodeWorking } from 'react-icons/io';
+import { FaBusinessTime, FaStar, FaTimeline } from 'react-icons/fa6';
+import { TiMessages } from 'react-icons/ti';
+import { GiAngelWings } from 'react-icons/gi';
+
+const ΠλαϊνήΣτήληΠρογραμματισμού = ({ εικονίδια }) => {
+  return (
+    <nav className={styles['βασική-πλαϊνή-στήλη-πλοήγησης']}>
+      <ΠλαίσιοΜορφής
+        καθορισμόςΔιαστάσεων="από μέσα"
+        ακτίναΓωνίας={15}
+        ενεργόςΔιακόπτηςΦέγγους={false}
+      >
+        <ul className={styles['στοιχεία']}>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatEndsWith href="/programming">
+              <GiAngelWings />
+            </NavLinkThatEndsWith>
+          </li>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatStartsWith href="/programming/portfolio">
+              <IoMdCodeWorking />
+            </NavLinkThatStartsWith>
+          </li>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatStartsWith href="/programming/timeline">
+              <FaTimeline style={{ transform: 'rotate(90deg)' }} />
+            </NavLinkThatStartsWith>
+          </li>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatStartsWith href="/programming/services">
+              <FaBusinessTime />
+            </NavLinkThatStartsWith>
+          </li>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatStartsWith href="/programming/testimonials">
+              <FaStar />
+            </NavLinkThatStartsWith>
+          </li>
+          <li className={styles['στοιχείο']}>
+            <NavLinkThatStartsWith href="/programming/contact" s>
+              <TiMessages />
+            </NavLinkThatStartsWith>
+          </li>
+        </ul>
+      </ΠλαίσιοΜορφής>
+    </nav>
+  );
+};
+export default ΠλαϊνήΣτήληΠρογραμματισμού;
