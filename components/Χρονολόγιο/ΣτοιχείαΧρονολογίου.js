0ce27efa8f4f5e4c87f3a599187c2b227279f8cb
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-23 19:11:08 +0100

    Update ΣτοιχείαΧρονολογίου.js

--- components/Χρονολόγιο/ΣτοιχείαΧρονολογίου.js
+++ components/Χρονολόγιο/ΣτοιχείαΧρονολογίου.js
@@ -4,13 +4,27 @@ import styles from '@/components/Χρονολόγιο/ΣτοιχείαΧρονο
 
 import ΦίλτραΓεγονότων from '@/components/Χρονολόγιο/ΦίλτραΓεγονότων';
 
+import { useAppContext } from '@/context/AppContext';
+
 const ΣτοιχείαΧρονολογίου = () => {
+  const { μορφήΣτοιχείων } = useAppContext();
+
   return (
     <>
-      <div className={styles['στοιχεία-χρονολογίου']}>
+      <div
+        className={
+          styles['στοιχεία-χρονολογίου'] +
+          ' ' +
+          styles[
+            `${μορφήΣτοιχείων === 'υαλομορφισμός' ? '' : 'με-θολό-κάλυμμα'}`
+          ]
+        }
+      >
         <ΦίλτραΓεγονότων />
       </div>
     </>
   );
 };
 export default ΣτοιχείαΧρονολογίου;
+
+
