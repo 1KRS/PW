56c4e5ca8431c3e85036ca4643c83c96f25d7211
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-24 22:07:54 +0100

    Create μαρτυρίες.js

---
+++ db/μαρτυρίες.js
@@ -0,0 +1,38 @@
+export const μαρτυρίες = [
+  {
+    id: 1,
+    όνομα: 'Αλέξανδρος',
+    βαθμολογία: 5,
+    κείμενο:
+      'Δεν μπορώ να περιγράψω το επίπεδο της συνεργασίας που είχα με τον Δημήτρη. Άριστος και επαγγελματίας σε όλα του.',
+    διεύθυνσηΕικόνας: '/images/mina-rad-K9T9hdf4PmI-unsplash.jpg',
+    κείμενοΕικόνας: 'Εικόνα Άνδρα',
+  },
+  {
+    id: 2,
+    όνομα: 'Υπατία',
+    βαθμολογία: 5,
+    κείμενο:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus. Pellentesque aliquam ac purus ac sagittis. Nullam gravida.',
+    διεύθυνσηΕικόνας: '/images/γυναίκα.png',
+    κείμενοΕικόνας: 'Εικόνα Γυναίκας',
+  },
+  {
+    id: 3,
+    όνομα: 'Ηρακλής',
+    βαθμολογία: 5,
+    κείμενο:
+      'Δεν μπορώ να περιγράψω το επίπεδο της συνεργασίας που είχα με τον Δημήτρη. Άριστος και επαγγελματίας σε όλα του.',
+    διεύθυνσηΕικόνας: '/images/mina-rad-K9T9hdf4PmI-unsplash.jpg',
+    κείμενοΕικόνας: 'Εικόνα Άνδρα',
+  },
+  {
+    id: 4,
+    όνομα: 'Άρτεμις',
+    βαθμολογία: 5,
+    κείμενο:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus. Pellentesque aliquam ac purus ac sagittis. Nullam gravida.',
+    διεύθυνσηΕικόνας: '/images/γυναίκα.png',
+    κείμενοΕικόνας: 'Εικόνα Γυναίκας',
+  },
+];
