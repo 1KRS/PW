cededecca103300fe31d18ee4bfcd495d16efc62
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-02-24 22:07:26 +0100

    Update page.js

--- app/programming/testimonials/page.js
+++ app/programming/testimonials/page.js
@@ -1,8 +1,11 @@
 import styles from './page.module.css';
 
-import Heading from '@/components/Heading';
 import Image from 'next/image';
+import Heading from '@/components/Heading';
+import Μαρτυρία from '@/components/Μαρτυρία';
 
+
+import { μαρτυρίες } from '@/db/μαρτυρίες';
 import { FaStar } from 'react-icons/fa';
 
 export const metadata = {
@@ -13,110 +16,30 @@ export const metadata = {
   publisher: 'Δημήτρης Μανωλόπουλος',
 };
 
-const Testimonials = () => {
+const Μαρτυρίες = () => {
   return (
-      <main className={styles.main}>
-        <Heading text="Μαρτυρίες" />
+    <main className={styles.main}>
+      <Heading text="Μαρτυρίες" />
 
-        <section className={styles.testimonials} id="testimonials">
-          <div className={styles['testimonials-container']}>
-            {/* <div className={styles.wrapper}> */}
-              <div className={styles['testimonial-item']}>
-                <Image
-                  className={styles.img}
-                  src={'/images/mina-rad-K9T9hdf4PmI-unsplash.jpg'}
-                  height={300}
-                  width={300}
-                  alt="Εικόνα Άνδρα"
-                />
-                <h2>Αλέξανδρος</h2>
-                <div className={styles.rating}>
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
-                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
-                </p>
-              </div>
-
-              <div className={styles['testimonial-item']}>
-                <Image
-                  className={styles.img}
-                  src={'/images/γυναίκα.png'}
-                  height={300}
-                  width={300}
-                  alt="Εικόνα Γυναίκας"
-                />
-                <h2>Υπατία</h2>
-                <div className={styles.rating}>
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
-                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
-                </p>
-              </div>
-
-              <div className={styles['testimonial-item']}>
-                <Image
-                  className={styles.img}
-                  src={'/images/mina-rad-K9T9hdf4PmI-unsplash.jpg'}
-                  height={300}
-                  width={300}
-                  alt="Εικόνα Άνδρα"
-                />
-                <h2>Σωκράτης</h2>
-                <div className={styles.rating}>
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
-                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
-                </p>
-              </div>
-
-              <div className={styles['testimonial-item']}>
-                <Image
-                  className={styles.img}
-                  src={'/images/γυναίκα.png'}
-                  height={300}
-                  width={300}
-                  alt="Εικόνα Γυναίκας"
-                />
-                <h2>Αθηνά</h2>
-                <div className={styles.rating}>
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus non lectus dictum, dapibus ipsum in, lacinia lectus.
-                  Pellentesque aliquam ac purus ac sagittis. Nullam gravida.
-                </p>
-              </div>
-            {/* </div> */}
-          </div>
-        </section>
-      </main>
+      <section className={styles['τομέας-μαρτυριών']} id="τομέας-μαρτυριών">
+        <div className={styles['περιέκτης-μαρτυριών']}>
+          {/* <div className={styles.wrapper}> */}
+          {μαρτυρίες.map((μαρτυρία) => (
+            <Μαρτυρία
+              key={μαρτυρία.id}
+              id={μαρτυρία.id}
+              όνομα={μαρτυρία.όνομα}
+              βαθμολογία={μαρτυρία.βαθμολογία}
+              κείμενο={μαρτυρία.κείμενο}
+              διεύθυνσηΕικόνας={μαρτυρία.διεύθυνσηΕικόνας}
+              κείμενοΕικόνας={μαρτυρία.κείμενοΕικόνας}
+            />
+          ))}
+          {/* </div> */}
+        </div>
+      </section>
+    </main>
   );
 };
 
-export default Testimonials;
+export default Μαρτυρίες;
