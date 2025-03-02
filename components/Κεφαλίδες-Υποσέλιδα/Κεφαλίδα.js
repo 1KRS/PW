a3700b01ae0c4b174cc49409907c97a520ae0fab
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-03-02 09:27:50 +0100

    Update Κεφαλίδα.js

--- components/Κεφαλίδες-Υποσέλιδα/Κεφαλίδα.js
+++ components/Κεφαλίδες-Υποσέλιδα/Κεφαλίδα.js
@@ -1,19 +1,26 @@
 import styles from './Κεφαλίδα.module.css';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import LanguagesDropdown from '../LanguagesDropdown';
 
 const Κεφαλίδα = () => {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <div>
-          Δημήτρης <span>Μανωλόπουλος</span>
-        </div>
-      </Link>
-      <nav className={styles['home-top-nav']}>
-        <ul className={styles.items}>
+    <header className={styles.κεφαλίδα}>
+      <div className={styles['περιέκτης-λογότυπου']}>
+        <Link href="/" className={styles.λογότυπο}>
+          <Image
+            className={styles.εικόνα}
+            src={'/images/Λογότυπο.svg'}
+            height={25}
+            width={30}
+            alt="Δημήτρης Μανωλόπουλος"
+          />
+        </Link>
+      </div>
+      <nav className={styles['γραμμή-πλοήγησης-κεφαλίδας-προγραμματισμού']}>
+        <ul className={styles.στοιχεία}>
           <LanguagesDropdown />
         </ul>
       </nav>
