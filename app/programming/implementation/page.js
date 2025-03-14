2640ccef81a32b0e1b60f78e7a815a3a83d35597
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-03-14 22:22:50 +0100

    Update page.js

--- app/programming/implementation/page.js
+++ app/programming/implementation/page.js
@@ -1,21 +1,24 @@
 import styles from './page.module.css';
 import TodoItems from '@/components/TodoItems';
-import Heading from '@/components/Heading';
+import Τίτλος from '@/components/Τίτλος';
 import ProgrammingTimelineSideNav from '@/components/Navigations/ProgrammingTimelineSideNav';
 
 const Implementation = () => {
   return (
     <>
-    <main className={styles.main}>
-      <Heading text="Προς Υλοποίηση" />
-      <ProgrammingTimelineSideNav σελίδα='Προς υλοποίηση' ύπαρξηΦίλτρων='ανύπαρκτα'/>
-      <section className={styles.todos} id="todos">
-        <div className={styles['todos-container']}>
-          <TodoItems />
-        </div>
-      </section>
-    </main>
-  </>
+      <main className={styles.main}>
+        <Τίτλος text="Προς Υλοποίηση" />
+        <ProgrammingTimelineSideNav
+          σελίδα="Προς υλοποίηση"
+          ύπαρξηΦίλτρων="ανύπαρκτα"
+        />
+        <section className={styles.todos} id="todos">
+          <div className={styles['todos-container']}>
+            <TodoItems />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
