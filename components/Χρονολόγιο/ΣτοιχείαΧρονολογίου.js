575280d6295ecad781665701eee41eb40648aa2e
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-04-05 18:26:39 +0200

    Update ΣτοιχείαΧρονολογίου.js

--- components/Χρονολόγιο/ΣτοιχείαΧρονολογίου.js
+++ components/Χρονολόγιο/ΣτοιχείαΧρονολογίου.js
@@ -5,12 +5,23 @@ import styles from '@/components/Χρονολόγιο/ΣτοιχείαΧρονο
 import ΦίλτραΓεγονότων from '@/components/Χρονολόγιο/ΦίλτραΓεγονότων';
 
 import { useAppContext } from '@/context/AppContext';
+import TranslatedText from '../TranslatedText';
 
 const TimelineElements = () => {
   const { μορφήΣτοιχείων } = useAppContext();
 
   return (
     <>
+      <div className={styles['περιέκτης-κειμένου-αναμονής']}>
+        <div className={styles['κείμενο-αναμονής']}>
+          <TranslatedText>
+            Μετά από προτάσεις βελτίωσης επισκεπτών γίνονται οι απαραίτητες αλλαγές στο
+            χρονολόγιο. Οπότε προς το παρόν τα γεγονότα χρονολογίου είναι ορατά
+            μόνο σε υπολογιστές. Σύντομα θα γίνουν προσβάσιμα και στις υπόλοιπες
+            συσκευές. Ευχαριστώ για την κατανόηση.
+          </TranslatedText>
+        </div>
+      </div>
       <div
         className={
           styles['στοιχεία-χρονολογίου'] +
@@ -26,5 +37,3 @@ const TimelineElements = () => {
   );
 };
 export default TimelineElements;
-
-
