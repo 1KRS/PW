9100ebeb79b85e4aff6ecb8e5c23dc1c04e89839
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-05-15 16:54:59 +0200

    Update route.js

--- app/api/emails/route.js
+++ app/api/emails/route.js
@@ -5,14 +5,14 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.PW_RESEND_API_KEY);
 
 export async function POST(request, res) {
-  const { fullName, email, number, subject, text } = await request.json();
+  const { fullName, email, number, app, subject, text } = await request.json();
 
   try {
     const { results, error } = await resend.emails.send({
       from: 'message@d-man.dev',
       to: 'ik4r0s@icloud.com',
       subject: 'Νέο μήνυμα από προσωπικό ιστότοπο',
-      react: ΜνμΑπόΕπισκέπτες({ fullName, email, number, subject, text }),
+      react: ΜνμΑπόΕπισκέπτες({ fullName, email, number, app, subject, text }),
     });
 
     if (error) {
