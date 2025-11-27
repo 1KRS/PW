dbd1e30188b1444c15a66d46ca2078337eb7d136
ΙΚΑΡΟΣ <ik4r0s@icloud.com>
2025-11-27 23:09:21 +0100

    Update ΥπόβαθροΠρογραμματισμού.js

--- components/Backgrounds/ΥπόβαθροΠρογραμματισμού.js
+++ components/Backgrounds/ΥπόβαθροΠρογραμματισμού.js
@@ -5,15 +5,25 @@ import styles from './ΥπόβαθροΠρογραμματισμού.module.css'
 import ΔυαδικήΒροχή from './ΔυαδικήΒροχή';
 import { useAppContext } from '@/context/AppContext';
 
-const ProgrammingBackground = ({ screenType, fontSize, speed, children }) => {
+const ProgrammingBackground = ({
+  screenType: τύποςΟθόνης,
+  fontSize: μέγεθοςΓραμματοσειράς,
+  speed: ταχύτητα,
+  children,
+}) => {
   const { υπόβαθροΠρογραμματισμού } = useAppContext();
 
   return υπόβαθροΠρογραμματισμού === 'δυναμικό' ? (
     <>
       <ΔυαδικήΒροχή
-        screenType={screenType || 'normal'}
-        fontSize={fontSize || '14'}
-        speed={speed || '80'}
+        τύποςΟθόνης={τύποςΟθόνης}
+        μέγεθος={μέγεθοςΓραμματοσειράς && Number(μέγεθοςΓραμματοσειράς)}
+        ταχύτηταΒροχής={ταχύτητα && Number(ταχύτητα)}
+        καθυστέρησηΠρότασης={8000}
+        πιθανότηταΠρότασης={0.2}
+        χρώμαΠρότασης={'#ffbb00'}
+        απόσβεσηMs={2000}
+        μέγιστεςΕνεργέςΠροτάσεις={6}
       />
       {children}
     </>
